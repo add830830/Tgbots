9aa7fa885ddba11f7e65437a0963855ff0fd8ac7
Aditya Yadav <69784758+aditya-yadav-27@users.noreply.github.com>
2021-09-04 13:05:30 +0530

    Update pokedexbot.py
    1. Switched to PokeApi from pokebase
    2. Added a bunch of new details in the data command (will add more soon)

--- pokedexbot.py
+++ pokedexbot.py
@@ -1,10 +1,13 @@
+# imports
+import requests
+import json
 import html
-from pokebase.loaders import generation
 from telegram.ext import *
 from telegram import *
-import pokebase as pb
+import botinfo
 import random
 import logging
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
@@ -22,26 +25,33 @@ keyboard = [
     [
         InlineKeyboardButton("Stats", callback_data='4'),
         InlineKeyboardButton("Move", callback_data='5'),
-        InlineKeyboardButton("Coverage", callback_data='6')
+        InlineKeyboardButton("Cry", callback_data='6')
     ]
 ]
 reply_markup = InlineKeyboardMarkup(keyboard)
 help_text = "Here's the commands that bot supports: "
 
+keyboard3 = [[InlineKeyboardButton(
+    "Previous", callback_data='15'), InlineKeyboardButton("Next", callback_data='16')]]
+reply_markup3 = InlineKeyboardMarkup(keyboard3)
+
 
 def start(update, context):
-    keyboard= [
-    [InlineKeyboardButton("Add Pan to your group", url='http://t.me/Mr7PanBot?startgroup=true')],
-    [InlineKeyboardButton("Support Group", url='https://t.me/bgmiofficial_1'),InlineKeyboardButton("Updates Channel", url='https://t.me/bgmiupdates')],
-    [InlineKeyboardButton("Source code", url='https://github.com/aditya-yadav-27/TgPokedexbot')],
-    [InlineKeyboardButton("Help & commands", url='http://t.me/Mr7PanBot?start=help')]]
+    keyboard = [
+        [InlineKeyboardButton("Add Pan to your group",
+                              url='http://t.me/Mr7PanBot?startgroup=true')],
+        [InlineKeyboardButton("Support Group", url='https://t.me/bgmi_pubgnewstate'),
+         InlineKeyboardButton("Updates Channel", url='https://t.me/bgmi_pubgnewstate_updates')],
+        [InlineKeyboardButton(
+            "Source code", url='https://github.com/aditya-yadav-27/TgPokedexbot')],
+        [InlineKeyboardButton("Help & commands", url='http://t.me/Mr7PanBot?start=help')]]
     reply_markup = InlineKeyboardMarkup(keyboard)
     user = update.message.from_user
     logger.info("User %s started the conversation.", user.id)
     user = (update.message.from_user['first_name'])
     master = "<a href='https://t.me/aditya_yadav_27'>Aditya</a>"
     context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('rotomdex.png', 'rb'),
-                           caption=f"Hey {user}, I am The most advance pokedex bot of Telegram made by my master {master}\nType /help to see what I can do!!", reply_markup=reply_markup ,parse_mode=ParseMode.HTML)
+                           caption=f"Hey {user}, I am The most advance pokedex bot of Telegram made by my master {master}\nType /help to see what I can do!!", reply_markup=reply_markup, parse_mode=ParseMode.HTML)
 
 
 def help(update: Update, context: CallbackContext):
@@ -91,71 +101,99 @@ def data(update: Update, context: CallbackContext):
     user_says = ' '.join(context.args)
     logger.info(f"User %s used data command for pokemon {user_says}", user.id)
     try:
-        msg = context.bot.send_message(
-            chat_id=update.effective_chat.id, text="Fetching data...")
-        p1 = pb.pokemon(user_says.casefold())
-        abilities = ''
-        types = ''
-        for poketype in p1.types:
-            types = ', '.join(poketype.type.name.capitalize()
-                              for poketype in p1.types)
-        for stat in p1.stats:
-            result = ['{}: {}'.format(stat.stat.name.capitalize(), stat.base_stat)
-                      for stat in p1.stats]
-        for ability in p1.abilities:
-            abilities = ', '.join(ability.ability.name.capitalize()
-                                  for ability in p1.abilities)
+        context.bot.send_chat_action(
+            chat_id=update.effective_chat.id, action='typing')
+        url = "https://pokeapi.co/api/v2/pokemon/{}/"
+        final_url = url.format(user_says.casefold())
+        response = requests.get(final_url)
+        response = response.content
+        info = json.loads(response)
+        url1 = "https://pokeapi.co/api/v2/pokemon-species/{}/"
+        final_url1 = url1.format(user_says.casefold())
+        response1 = requests.get(final_url1)
+        response1 = response1.content
+        info1 = json.loads(response1)
 
+        gender_rate = info1['gender_rate']
+        egg_groups = info1['egg_groups']
+        counter = info1['hatch_counter']
+        catch_rate = info1['capture_rate']
+
+        abl = info['abilities']
+        all_types = info['types']
+        name = info['name']
+        id1 = info['id']
+        height = info['height']
+        weight = info['weight']
+        base_experience = info['base_experience']
+
+        groups = ''
+        types = ''
+        abilities = ''
+        for group in egg_groups:
+            groups = '/'.join(f"{group['name']}".capitalize()
+                              for group in egg_groups)
+        for abls in abl:
+            abilities = ', '.join(
+                f"{abls['ability']['name'].capitalize()}" for abls in abl)
+        for type in all_types:
+            types = ', '.join(
+                f"{type['type']['name'].capitalize()}" for type in all_types)
         proper = user_says.split('-')
         proper1 = proper[0]
         bulba_link = f'https://bulbapedia.bulbagarden.net/wiki/{proper1.capitalize()}_(Pok%C3%A9mon)'
         bulba2 = f"<a href='{bulba_link}'>Bulbapedia</a>"
-        caption = f'''<u><b>#{p1.id} {p1.name.capitalize()}</b></u>
-        <b>Types</b>
-        {types}
 
-        <b>Abilities</b>
-        {abilities}
+        caption = f'''<u><b>#{id1} | {name.capitalize()} | </b></u>
+            <b>Types</b>
+            {types}
 
-        <b>Gender Ratio</b>                <b>Catch Rate</b>
-        will add soon                      will add soon
-                            <b>Breeding</b>
-        <b>Egg groups</b>                  <b>Hatch Time</b>
-        will add soon                      will add soon
-               
-        <b>Height/Weight</b>
-        {p1.height/10}m/{p1.weight/10}kg
+            <b>Abilities</b>
+            {abilities}
 
-        <b>Base-exp</b>           <b>Base Freindship</b>
-        {p1.base_experience}      will add soon
+            <b>Gender Ratio</b>                <b>Catch Rate</b>
+            {gender_rate}/8                    {catch_rate}
 
-        <b>Evolution Line</b>
-        will add soon 
+            <b>Egg groups</b>                  <b>Hatch Time</b>
+            {groups}                           {255*(counter+1)}
 
-        <b>Other Forms</b>
-        will add soon
+            <b>Height/Weight</b>
+            {height/10}m/{weight/10}kg
 
-Read more about this pokemon on <b>{bulba2}</b>'''
+            <b>Base-exp</b>                    <b>EV yield</b>
+            {base_experience}                  will add soon
+
+            <b>Evolution Line</b>
+            will add soon
+
+            <b>Other Forms</b>
+            will add soon
+
+Read more about this pokemon on {bulba2}'''
 
         update.message.reply_document(
-            f"https://img.pokemondb.net/artwork/{p1.name}.jpg", caption=caption, parse_mode=ParseMode.HTML)
-        msg.delete()
+            f"https://img.pokemondb.net/artwork/{user_says.casefold()}.jpg", caption=caption, parse_mode=ParseMode.HTML, reply_markup=reply_markup3)
+
     except Exception as e:
         update.message.reply_text(
             f"'{user_says}' is not a valid pokemon! try again")
-        msg.delete()
 
 
 def randpoke(update: Update, context: CallbackContext):
     user = update.message.from_user
     logger.info("User %s used randpoke command", user.id)
-    l = context.bot.send_message(
-        chat_id=update.effective_chat.id, text='Selecting a random pokemon!...')
+    context.bot.send_chat_action(
+        chat_id=update.effective_chat.id, action='typing')
     k = random.randint(1, 898)
-    p2 = pb.pokemon(k)
+    url = "https://pokeapi.co/api/v2/pokemon/{}/"
+    final_url = url.format(k)
+    response = requests.get(final_url)
+    response = response.content
+    info = json.loads(response)
+    id1 = info['id']
+    name = info['name']
     context.bot.send_document(chat_id=update.effective_chat.id,
-                              document=f"http://play.pokemonshowdown.com/sprites/xyani/{p2.name}.gif", caption=f"<b>{p2.name.capitalize()} | #{p2.id}</b>")
-    l.delete()
+                              document=f"http://play.pokemonshowdown.com/sprites/xyani/{name}.gif", caption=f"<b>{name.capitalize()} | #{id1}</b>")
 
 
 def shiny(update: Update, context: CallbackContext):
@@ -163,55 +201,62 @@ def shiny(update: Update, context: CallbackContext):
     user_says = ' '.join(context.args)
     logger.info(f"User %s used shiny command for pokemon {user_says}", user.id)
     try:
-        msg = context.bot.send_message(
-            chat_id=update.effective_chat.id, text="Getting image...")
-        p1 = pb.pokemon(user_says.casefold())
+        url = "https://pokeapi.co/api/v2/pokemon/{}/"
+        final_url = url.format(user_says.casefold())
+        response = requests.get(final_url)
+        response = response.content
+        info = json.loads(response)
+        name = info['name']
+        id1 = info['id']
+        context.bot.send_chat_action(
+            chat_id=update.effective_chat.id, action="typing")
         update.message.reply_photo(
-            f"https://img.pokemondb.net/sprites/home/shiny-2x/jpg/{user_says}.jpg", caption=f"<b>#{p1.id} | Shiny {p1.name.capitalize()}</b>")
-        msg.delete()
+            f"https://img.pokemondb.net/sprites/home/shiny-2x/jpg/{user_says}.jpg", caption=f"<b>#{id1} | Shiny {name.capitalize()}</b>")
+
     except:
         context.bot.send_message(chat_id=update.effective_chat.id,
                                  text=f"{user_says} is not a valid pokemon, try again!")
-        msg.delete()
 
 
 def stats(update: Update, context: CallbackContext):
     user_says = ' '.join(context.args)
     user = update.message.from_user
     logger.info(f"User %s used stats command for pokemon {user_says}", user.id)
-    try:
-        msg = context.bot.send_message(
-            chat_id=update.effective_chat.id, text="Getting stats...")
-        p1 = pb.pokemon(user_says.casefold())
-        for stat in p1.stats:
-            result = ['{}: {}'.format(stat.stat.name.capitalize(), stat.base_stat)
-                      for stat in p1.stats]
-        hp = result[0]
-        attack = result[1]
-        defense = result[2]
-        sattack = result[3]
-        sdefense = result[4]
-        spd = result[5]
-        thp = int(hp[4:8])
-        tatk = int(attack[8:12])
-        tdef = int(defense[9:13])
-        tsat = int(sattack[16:20])
-        tsdef = int(sdefense[17:21])
-        tspd = int(spd[7:11])
-        update.message.reply_text(f'''<b><u>#{p1.id} | {p1.name.capitalize()}</u></b>
-            <code>{result[0]}
-        {result[1]}
-        {result[2]}
-        {result[3]}
-        {result[4]}
-        {result[5]}</code>
-        <b>Total: {thp+tatk+tdef+tsdef+tspd+tsat}</b>
-            ''', parse_mode=ParseMode.HTML)
-        msg.delete()
-    except:
-        context.bot.send_message(
-            chat_id=update.effective_chat.id, text=f'{user_says} is not a valid pokemon')
-        msg.delete()
+    # try:
+    context.bot.send_chat_action(
+        chat_id=update.effective_chat.id, action="typing")
+    url = "https://pokeapi.co/api/v2/pokemon/{}/"
+    final_url = url.format(user_says.casefold())
+    response = requests.get(final_url)
+    response = response.content
+    info = json.loads(response)
+    poke_name = info['name']
+    poke_name = str(poke_name).capitalize()
+    poke_id = info['id']
+    stata = info['stats']
+    name = ''
+    base_stat = ''
+    for details in stata:
+        base_stat = ', '.join(
+            f"{details['base_stat']}" for details in stata)
+    for details1 in stata:
+        name = ', '.join(
+            f"{details1['stat']['name']}"for details1 in stata)
+    name = name.split(', ')
+    base_stat = base_stat.split(', ')
+    update.message.reply_text(f'''<b><u>#{poke_id} | {poke_name}</u></b>
+    <code>  {str(name[0]).capitalize()}: {int(base_stat[0])}
+    {str(name[1]).capitalize()}: {int(base_stat[1])}
+    {str(name[2]).capitalize()}: {int(base_stat[2])}
+    {str(name[3]).capitalize()}: {int(base_stat[3])}
+    {str(name[4]).capitalize()}: {int(base_stat[4])}
+    {str(name[5]).capitalize()}: {int(base_stat[5])}</code>
+    <b>Total: {int(base_stat[0])+int(base_stat[1])+int(base_stat[2])+int(base_stat[3])+int(base_stat[4])+int(base_stat[5])}</b>
+        ''', parse_mode=ParseMode.HTML)
+
+    # except Exception as e:
+    #     context.bot.send_message(
+    #         chat_id=update.effective_chat.id, text=f'"{user_says}" is not a valid pokemon! try again {e}')
 
 
 def move(update: Update, context: CallbackContext):
@@ -221,33 +266,61 @@ def move(update: Update, context: CallbackContext):
         f"User %s used move command for move/pokemon {user_says}", user.id)
 
     try:
-        msg = context.bot.send_message(
-            chat_id=update.effective_chat.id, text=f'Getting move details...')
-        p1 = pb.move(user_says)
-        generation = str(p1.generation).split('-')
-        generation = generation[0].capitalize()+'-'+generation[1].upper()
-        update.message.reply_text(f'''<b><u>{p1.id} | {str(p1.name.capitalize())} | {generation}</u></b>
-        <b>Type</b>                               <b>Category</b>
-        {str(p1.type.name).capitalize()}                             {str(p1.damage_class).capitalize()}
+        context.bot.send_chat_action(
+            chat_id=update.effective_chat.id, action='typing')
+        url = "https://pokeapi.co/api/v2/move/{}/"
+        final_url = url.format(user_says.casefold())
+        response = requests.get(final_url)
+        response = response.content
+        info = json.loads(response)
 
-        <b>Power Points</b>                <b>Base Power</b>
-        {p1.pp}                                      {p1.power}
+        # getting data from the api
+        id1 = info['id']
+        name = info['name']
+        accuracy = info['accuracy']
+        pp = info['pp']
+        priority = info['priority']
+        power = info['power']
+        generation = info['generation']
+        effect_entries = info['effect_entries']
+        effect_chance = info['effect_chance']
+        type1 = info['type']
+        category = info['damage_class']
+        target = info['target']
+        contest = info['contest_type']
+        entry = ''
+        for effect in effect_entries:
+            entry += f"{effect['short_effect']}"
+        ec = '$effect_chance%'
+        if ec in entry:
+            entry = entry.replace(ec, f'{effect_chance}%')
+        else:
+            pass
+            # formating data
+        update.message.reply_text(f'''<b><u>{id1} | {name.capitalize()} | {generation['name'].capitalize()}</u></b>
+            <b>Type</b>                        <b>Category</b>
+            {type1['name'].capitalize()}       {category['name'].capitalize()}
 
-        <b>Accuracy</b>                    <b>Target</b>
-        {p1.accuracy}%                     {p1.target}
+            <b>Power Points</b>                <b>Base Power</b>
+            {pp}                               {power}
 
-        <b>Contest</b>
-        {str(p1.contest_type).capitalize()}
-        ''', parse_mode=ParseMode.HTML)
-        msg.delete()
+            <b>Accuracy</b>                    <b>Target</b>
+            {accuracy}%                        {target['name'].capitalize()}
+
+            <b>Contest</b>                     <b>Priority</b>
+            {contest['name'].capitalize()}     {priority}
+
+            <b>Description</b>
+            {entry}
+            ''', parse_mode=ParseMode.HTML)
+
     except Exception as e:
         context.bot.send_message(
             chat_id=update.effective_chat.id, text=f'{user_says} is not a valid move try again {e}')
-        msg.delete()
 
 
 def main():
-    updater = Updater(token='TOKEN)
+    updater = Updater(token=botinfo.token)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(CommandHandler('help', help))
