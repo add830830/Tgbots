89d6f20c148bf1fa327cff4cc62e673c30046e1c
Aditya Yadav <69784758+aditya-yadav-27@users.noreply.github.com>
2021-10-01 17:16:42 +0530

    Update giveawaybot.py
    Major update :)

--- giveawaybot.py
+++ giveawaybot.py
@@ -7,11 +7,11 @@ import telegram, telegram.ext, random, logging, datetime, asyncio, html, uuid
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
-u = Updater('TOKEN', use_context=True)
+u = Updater('1970002293:AAEB-wJBA0_o5OhzldopqTspa_N8cZtLKLs', use_context=True)
 
 #setting vars
 j = u.job_queue
-k=''
+items={}
 users=[]
 users = list(dict.fromkeys(users))
 
@@ -24,7 +24,7 @@ def start(update: Update, context:CallbackContext):
 
 <b>Hold giveaways quickly and easily!</b>
 
-Hello! I'm <b>GiveawayBot</b>, and I'm here to make it as easy as possible to hold giveaways on your Telegram group/channel! I was created by <a href='https://t.me/aditya_yadav_27'>Aditya</a> <code>(1383570275)</code> using the <a href='https://github.com/python-telegram-bot/python-telegram-bot'>Python-telegram-bot</a> library (13.7) Check out my commands by typing /ghelp''', parse_mode=ParseMode.HTML)
+Hello! I'm <b>GiveawayBot</b>, and I'm here to make it as easy as possible to hold giveaways on your Telegram group/channel! I was created by <a href='tg://user?id=1383570275'>Aditya</a> <code>(1383570275)</code> using the <a href='https://github.com/python-telegram-bot/python-telegram-bot'>Python-telegram-bot</a> library (13.7) Check out my commands by typing /ghelp''', parse_mode=ParseMode.HTML, disable_web_page_preview=True)
 def ghelp(update: Update, context:CallbackContext):
     if update.effective_chat.type=='private': 
         context.bot.send_message(chat_id=update.effective_chat.id, text=f'''Here's the commands that the bot supports: 
@@ -32,29 +32,24 @@ def ghelp(update: Update, context:CallbackContext):
 <b>!gend [messageId]</b> - ends (picks a winner for) the specified or latest giveaway in the current channel
 <b>!greroll [messageId]</b> - re-rolls the specified or latest giveaway in the current channel
 <b>!glist</b> - lists active giveaways on the server
-
 Do not include {html.escape('<>')} nor [] - {html.escape('<>')} means required and [] means optional.
 ''', parse_mode=ParseMode.HTML)
     else:
         keyboard=[[InlineKeyboardButton(text='Click here for help!', url='http://t.me/dctggiveawaybot?start=ghelp')]]
         context.bot.send_message(chat_id=update.effective_chat.id, text=f"Contact me in PM to get the list of available commands.", reply_markup=InlineKeyboardMarkup(keyboard))
 
-
 def button(update: Update, context: CallbackContext):
     query = update.callback_query
     choice = query.data
     if choice=='1':
-        user=users.append(update.callback_query.from_user.id)
-        update.callback_query.answer(text='Prticipation successful!', show_alert=True)
-    try:
-        global k
-        k=random.randint(0, len(user))
-    except Exception as e:
-        print(f'kuch to gadbad h daya {e}')
+        members=context.bot.get_chat_member(chat_id=update.effective_chat.id, user_id=update.callback_query.from_user.id)
+        if members.status==telegram.ChatMemberAdministrator or telegram.ChatMemberOwner or telegram.ChatMemberMember or telegram.ChatMemberRestricted:
+            users.append(update.callback_query.from_user.id)
+            update.callback_query.answer(text='Prticipation successful!', show_alert=True)
+        else:
+            update.callback_query.answer(text='Join the group to participate', show_alert=True)
+        print(members.status)
 
-#giveaway end result
-def confirmation(update: Update, context: telegram.ext.CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=f'congrats @{users[k]} you won now gib party')
 
 #giveaway start
 def gstart(update: Update, context: CallbackContext):
@@ -68,23 +63,31 @@ def gstart(update: Update, context: CallbackContext):
         seconds=False
         minutes=False
         hours=False
-        weeks=False
+        days=False
         chat_id=update.effective_chat.id
+        uid=uuid.uuid4()
         try:
             tym=user_says[0]
             winners=user_says[1]
-            item=user_says[2:]
-            item = ' '.join(item)
-    
+            items[uid] = {"id": chat_id, "item": user_says[2:]}
+            item=str(items[uid]["item"])
+            item=item.replace('[', '')
+            item=item.replace(']', '')
+            item=item.replace("'", '')
+
+        except:
+            context.bot.send_message(chat_id=chat_id, text=f'''💥 Please include a length of time, and a number of winners and a prize! Example usage: <code>/gstart 30m 5w Awesome T-Shirt</code>''', parse_mode=ParseMode.HTML)
+        try:
             #winner
             if winners.endswith('w' or 'W'):
                 winners=winners.split('w' or 'W')
                 winners=int(winners[0])
-            else:
-                context.bot.send_message(chat_id=chat_id, text=f'''💥 Failed to parse winners from <code>{winners}</code>
+            elif winners<1:
+                context.bot.send_message(chat_id=chat_id, text=f'''Winners cannot be less than 1''', parse_mode=ParseMode.HTML)
+        except:    
+            context.bot.send_message(chat_id=chat_id, text=f'''💥 Failed to parse winners from <code>{winners}</code>
 Example usage: <code>/gstart 30m 5w Awesome T-Shirt</code>''', parse_mode=ParseMode.HTML)
-            if winners<1:
-                context.bot.send_message(chat_id=chat_id, text='Winners can not be less than 1')
+        try:
             #time
             if tym.endswith('s' or 'S'):
                 seconds=True
@@ -104,16 +107,16 @@ Example usage: <code>/gstart 30m 5w Awesome T-Shirt</code>''', parse_mode=ParseM
                 added=now+datetime.timedelta(hours=int(tym[0]))
                 tym1=tym[0]
                 tym=f'{tym[0]} Hours'
-            elif tym.endswith('w' or 'W'):
-                weeks=True
-                tym=tym.split('w' or 'W')
-                added=now+datetime.timedelta(weeks=int(tym[0]))
+            elif tym.endswith('d' or 'D'):
+                days=True
+                tym=tym.split('d' or 'D')
+                added=now+datetime.timedelta(days=int(tym[0]))
                 tym1=tym[0]
-                tym=f'{tym[0]} Weeks'
-            else:
-                context.bot.send_message(chat_id=chat_id, text=f'''💥 Failed to parse time from <code>{tym}</code>
+                tym=f'{tym[0]} days'
+        except:
+            context.bot.send_message(chat_id=chat_id, text=f'''💥 Failed to parse time from <code>{tym}</code>
 Example usage: <code>/gstart 30m 5w Awesome T-Shirt</code>''', parse_mode=ParseMode.HTML)
-
+        try:
             if now.strftime('%d %m %Y')==added.strftime('%d %m %Y'):
                 end=added.strftime('Today at %I:%M %p')
             else:
@@ -125,23 +128,34 @@ React with 🎉 to enter!
 Ends: in {tym} ({added.strftime("%B %d, %Y %I:%M %p")})
 Hosted by: @{update.effective_user.username}
 {winners} Winner(s) | Ends at • {end}
-Giveaway id: <code>{uuid.uuid4()}</code>
+Giveaway id: <code>{uid}</code>
         ''', parse_mode=ParseMode.HTML, reply_markup=InlineKeyboardMarkup(keyboard))
             tym1=int(tym1)
             if seconds==True:
-                j.run_once(confirmation, tym1)
+                j.run_once(callback, tym1, context=uid)
             elif minutes==True:
-                j.run_once(confirmation, tym1*60)
+                j.run_once(callback, tym1*60, context=uid)
             elif hours==True:
-                j.run_once(confirmation, tym1*60*60)
-            elif weeks==True:
-                j.run_once(confirmation, tym1*60*60*24*7)
+                j.run_once(callback, tym1*60*60, context=uid)
+            elif days==True:
+                j.run_once(callback, tym1*60*60*24, context=uid)
             else:
                 pass
             print('successful')
-        except:
-            context.bot.send_message(chat_id=chat_id, text=f'''💥 Please include a length of time, and a number of winners and a prize! Example usage: <code>/gstart 30m 5w Awesome T-Shirt</code>''', parse_mode=ParseMode.HTML)
-            
+        except Exception as e:
+            print(f'unsuccesful {e}')
+
+  
+#giveaway end result
+def callback(context: telegram.ext.CallbackContext):
+    uid = context.job.context
+    chat_id = items[uid]["id"]
+    item=str(items[uid]["item"])
+    item=item.replace('[', '')
+    item=item.replace(']', '')
+    item=item.replace("'", '')
+    context.bot.send_message(chat_id=chat_id, text=f'Great you won {item}')
+    del items[uid]
 
 if __name__ == '__main__':
     dp = u.dispatcher
